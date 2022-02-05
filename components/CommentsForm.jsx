65d43fd5e5ffdb69fa9f70e7c98a27c4cd1ef525
LilyPichew <mtaufikfieri@gmail.com>
2022-02-06 02:34:25 +0700

    first commit

---
+++ components/CommentsForm.jsx
@@ -0,0 +1,129 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+import { submitComment } from '../services'
+
+const CommentsForm = ({ slug }) => {
+  /* useState */
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+  /* useRef */
+  const commentEl = useRef()
+  const nameEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
+  /* useEffect */
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name')
+    emailEl.current.value = window.localStorage.getItem('email')
+  }, [])
+
+  const handleCommentSubmission = () => {
+    setError(false)
+
+    /* Value before and after comment */
+    const { value: comment } = commentEl.current
+    const { value: name } = nameEl.current
+    const { value: email } = emailEl.current
+    const { checked: storeData } = storeDataEl.current
+
+    /* Validations */
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+
+    /* Object of the data that we want to store */
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    }
+
+    if (storeData) {
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
+    } else {
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
+    }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true)
+      setTimeout(() => {
+        setShowSuccessMessage(false)
+      }, 3000)
+    })
+  }
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Leave a Reply
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          ref={nameEl}
+          className="w-full rounded-lg bg-gray-100 px-4 py-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          ref={emailEl}
+          className="w-full rounded-lg bg-gray-100 px-4 py-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <div>
+          <input
+            ref={storeDataEl}
+            type="checkbox"
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className="ml-2 cursor-pointer text-gray-500"
+            htmlFor="storeData"
+          >
+            Save my e-mail and name for the next time I comment.
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          className="ease inline-block cursor-pointer rounded-full bg-blue-300 py-3 px-8 text-lg text-white shadow-lg transition duration-500 hover:-translate-y-2 hover:bg-green-400"
+          type="button"
+          onClick={handleCommentSubmission}
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default CommentsForm
